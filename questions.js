15ef33477e3105c67f5a1d136b87c989b60a94cf
AnaDaniela96 <108939830+AnaDaniela96@users.noreply.github.com>
2024-07-08 13:36:11 -0600

    add questions JSON format.

--- questions.js
+++ questions.js
@@ -1,36 +1,94 @@
 // Preguntas para la trivia
-export const questionnaire = [
-  {
-    categoria: "Cine chickflick",
-    pregunta: "¿Cómo vddddes a Dua Lipa?",
-    respuesta: "Excelente.",
-    incorrecta1: "Muy mala amiga",
-    incorrecta2: "Era demasiado alta",
-    incorrecta3: "YOLO, no más xq sí",
-  },
-  {
-    categoria: "Cine chickflick",
-    pregunta: "¿Cómo ves a Dua Lipa",
-    respuesta: "Excelenfffte.",
-    incorrecta1: "Muy malddda amiga",
-    incorrecta2: "Era demasiado alta",
-    incorrecta3: "YOLO, no más xq sí",
-  },
-  {
-    categoria: "Cine chickflick",
-    pregunta: "¿Cómo ves a Dua Lipa",
-    respuesta: "Excelente.",
-    incorrecta1: "Muy mala amiga",
-    incorrecta2: "Era demasiado alta",
-    incorrecta3: "YOLO, no más xq sí",
-  },
-  {
-    categoria: "Cine chickflick",
-    pregunta: "¿Cómo ves a Dua Lipa",
-    respuesta: "Excelente.",
-    incorrecta1: "Muy mala amiga",
-    incorrecta2: "Era demasiado alta",
-    incorrecta3: "YOLO, no más xq sí",
-  },
+export const questions = [
+  
+    {
+        "question": "¿En qué película de 1999, protagonizada por Julia Stiles y Heath Ledger, se basa en la obra 'La fierecilla domada' de Shakespeare?",
+        "option1": "Clueless",
+        "option2": "10 Things I Hate About You",
+        "option3": "She's All That",
+        "option4": "Never Been Kissed",
+        "image": "link_a_la_imagen1",
+        "correct": "10 Things I Hate About You"
+    },
+    {
+        "question": "¿Qué actriz protagoniza la película de 1995 'Clueless'?",
+        "option1": "Reese Witherspoon",
+        "option2": "Drew Barrymore",
+        "option3": "Alicia Silverstone",
+        "option4": "Sarah Michelle Gellar",
+        "image": "link_a_la_imagen2",
+        "correct": "Alicia Silverstone"
+    },
+    {
+        "question": "¿En qué película de 2001 Reese Witherspoon interpreta a una estudiante de leyes en Harvard?",
+        "option1": "Legally Blonde",
+        "option2": "Sweet Home Alabama",
+        "option3": "Cruel Intentions",
+        "option4": "Election",
+        "image": "link_a_la_imagen3",
+        "correct": "Legally Blonde"
+    },
+    {
+        "question": "¿Cuál es el nombre del personaje de Lindsay Lohan en 'Mean Girls' (2004)?",
+        "option1": "Karen Smith",
+        "option2": "Cady Heron",
+        "option3": "Regina George",
+        "option4": "Gretchen Wieners",
+        "image": "link_a_la_imagen4",
+        "correct": "Cady Heron"
+    },
+    {
+        "question": "¿Qué película de 1995 está basada en la novela de Jane Austen 'Emma'?",
+        "option1": "She's All That",
+        "option2": "10 Things I Hate About You",
+        "option3": "Clueless",
+        "option4": "Never Been Kissed",
+        "image": "link_a_la_imagen5",
+        "correct": "Clueless"
+    },
+    {
+        "question": "¿Quién protagoniza la película 'She's All That' (1999)?",
+        "option1": "Freddie Prinze Jr.",
+        "option2": "Heath Ledger",
+        "option3": "Paul Walker",
+        "option4": "Ryan Phillippe",
+        "image": "link_a_la_imagen6",
+        "correct": "Freddie Prinze Jr."
+    },
+    {
+        "question": "¿En qué película de 2002 Jennifer Garner desea tener 30 años?",
+        "option1": "13 Going on 30",
+        "option2": "Never Been Kissed",
+        "option3": "Sweet Home Alabama",
+        "option4": "The Princess Diaries",
+        "image": "link_a_la_imagen7",
+        "correct": "13 Going on 30"
+    },
+    {
+        "question": "¿Qué película de 1999 presenta un baile memorable al ritmo de 'Rockafeller Skank' de Fatboy Slim?",
+        "option1": "10 Things I Hate About You",
+        "option2": "She's All That",
+        "option3": "Clueless",
+        "option4": "Never Been Kissed",
+        "image": "link_a_la_imagen8",
+        "correct": "She's All That"
+    },
+    {
+        "question": "¿Qué actriz interpreta a la mejor amiga del personaje de Anne Hathaway en 'The Princess Diaries' (2001)?",
+        "option1": "Mandy Moore",
+        "option2": "Julie Andrews",
+        "option3": "Heather Matarazzo",
+        "option4": "Sandra Oh",
+        "image": "link_a_la_imagen9",
+        "correct": "Heather Matarazzo"
+    },
+    {
+        "question": "¿En qué película de 2003 Amanda Bynes se hace pasar por su hermano en una escuela de élite?",
+        "option1": "She's the Man",
+        "option2": "What a Girl Wants",
+        "option3": "Sydney White",
+        "option4": "Easy A",
+        "image": "link_a_la_imagen10",
+        "correct": "She's the Man"
+    }
 ];
-
